a97315b09505735ee89eff9c38b4093fa685cc27
Kaustubh Padwal <kaustubhpadwal@kaustubhs-macbook-air.home>
2023-02-02 19:25:43 +0100

    404 page added

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,32 @@
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { StrikeThrough } from "../../commonStyles";
+import ButtonOutlined from "../../components/ButtonOutlined";
+import ContentText from "../../components/ContentText";
 import Page from "../../components/Page";
 
 const NotFound = () => {
-  return <Page></Page>;
+  return (
+    <Page>
+      <Box m={5}>
+        <ContentText textVariant="subHeading" color="#A4A8AE" mb={3}>
+          &gt;_ <StrikeThrough>Page not found!</StrikeThrough>
+        </ContentText>
+        <ContentText textVariant="body" color="#A4A8AE" ml={7.5}>
+          <StrikeThrough>
+            The page you're looking for does not exist...
+          </StrikeThrough>
+        </ContentText>
+        <Box mt={6} ml={7.5}>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <ButtonOutlined>
+              <StrikeThrough>Back to the home page</StrikeThrough>
+            </ButtonOutlined>
+          </Link>
+        </Box>
+      </Box>
+    </Page>
+  );
 };
 
 export default NotFound;
